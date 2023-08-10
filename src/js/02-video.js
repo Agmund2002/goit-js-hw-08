a9856ac210afbcd6341079d944f858aa8062dcba
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-10 21:54:24 +0300

    Completed 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(handlerPlayer, 1000));
+
+function handlerPlayer(evt) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(Math.floor(evt.seconds)));
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0);
