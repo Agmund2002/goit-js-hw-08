0323e2244a3ce3e05b29967e5ba8d88f2ebcaacf
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-11 19:04:06 +0300

    Completed 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,12 +2,14 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
+const storageKey  = "videoplayer-current-time";
+
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(handlerPlayer, 1000));
 
 function handlerPlayer(evt) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(Math.floor(evt.seconds)));
+    localStorage.setItem(storageKey, JSON.stringify(Math.floor(evt.seconds)));
 }
 
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0);
+player.setCurrentTime(JSON.parse(localStorage.getItem(storageKey)) ?? 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form')
+const storageKey = "feedback-form-state";
+
+form.addEventListener('input', throttle(handlerForm, 500));
+
+function handlerForm(evt) {
+    const {email, message} = evt.currentTarget.elements;
+
+    const inputs = {
+        email: email.value,
+        message: message.value
+    }
+
+    localStorage.setItem(storageKey, JSON.stringify(inputs));
+}
+
+const inputsParse = JSON.parse(localStorage.getItem(storageKey));
+
+form.elements.email.value = inputsParse.email ?? '';
+form.elements.message.value = inputsParse.message ?? '';
+
+// Функціонал сабміту
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    
+    console.log(inputsParse);
+    
+    evt.currentTarget.reset();
+    localStorage.clear();
+}
