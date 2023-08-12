bd17f259cfc50f1f5bfd45d3d362aed5536ded62
Agmund2002 <malysh.andrey2000@gmail.com>
2023-08-12 17:50:36 +0300

    debug

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,23 +3,25 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form')
 const storageKey = "feedback-form-state";
 
+const data = JSON.parse(localStorage.getItem(storageKey));
+
+if (data) {
+    form.elements.email.value = data.email;
+    form.elements.message.value = data.message;
+    } 
+
 form.addEventListener('input', throttle(handlerForm, 500));
 
-function handlerForm(evt) {
-    const {email, message} = evt.currentTarget.elements;
-
+function handlerForm() {
+    const {email, message} = form.elements;
+    
     const inputs = {
         email: email.value,
         message: message.value
     }
-
-    localStorage.setItem(storageKey, JSON.stringify(inputs));
+    
+    localStorage.setItem(storageKey, JSON.stringify(inputs))
 }
-
-const inputsParse = JSON.parse(localStorage.getItem(storageKey));
-
-form.elements.email.value = inputsParse.email ?? '';
-form.elements.message.value = inputsParse.message ?? '';
 
 // Функціонал сабміту
 
@@ -28,9 +30,8 @@ form.addEventListener('submit', handlerSubmit);
 function handlerSubmit(evt) {
     evt.preventDefault();
     
-    console.log(inputsParse);
+    console.log(JSON.parse(localStorage.getItem(storageKey)));
     
     evt.currentTarget.reset();
     localStorage.clear();
 }
-
